81c3be69f311e8b13ea21b7b93208b92346d8b74
xieyan <971387298@qq.com>
2015-12-31 16:37:49 +0800

    change:添加ConfigedControls框架

---
+++ Web/Common/BasedUserControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eIvy.Web.UI.ConfigedControls;
+
+namespace SyncServer.Web.Common
+{
+    public abstract class BasedUserControl : BasedConfigedControl
+    {
+    }
+}
