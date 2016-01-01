dd83e71c83ff06fcd94152283a8b5e947fc3be33
xieyan <971387298@qq.com>
2016-01-02 00:09:32 +0800

    change:修改登录bug（还有部分问题）

---
+++ Web/Common/UserControl/LeftMenuSiteMap.ascx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SyncServer.Web.Common;
+
+namespace SCMS.Web.Common.UserControl
+{
+    public partial class LeftMenuSiteMap : System.Web.UI.UserControl
+    {
+        private string _currentUserName;
+        /// <summary>
+        /// 当前登录用户的名称;
+        /// </summary>
+        protected string CurrentUserName
+        {
+            get
+            {
+                if (HttpContext.Current != null
+                        && HttpContext.Current.User != null
+                            && HttpContext.Current.User.Identity != null
+                                && !string.IsNullOrEmpty(HttpContext.Current.User.Identity.Name))
+                    _currentUserName = HttpContext.Current.User.Identity.Name.Trim();
+                return _currentUserName;
+            }
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string currentRoleName = string.Empty;
+            string[] r = System.Web.Security.Roles.GetRolesForUser(CurrentUserName);
+            try
+            {
+                switch (r.Length)
+                {
+                    case 1: currentRoleName = r[0]; break;
+                    default: currentRoleName = r.First(); break;
+                }
+            }
+            catch (Exception)
+            { Response.Redirect("~/Login.aspx"); }
+
+            switch (currentRoleName)
+            {
+                case SYNCRoleName.User: this.SiteMapDataSource1.SiteMapProvider = "Administrator"; break;
+                //case SCMSRoleName.Teacher: this.SiteMapDataSource1.SiteMapProvider = "Teacher"; break;
+                //case SCMSRoleName.Student: this.SiteMapDataSource1.SiteMapProvider = "Students"; break;
+                // case SCMSRoleName.DepartmentManager: this.SiteMapDataSource1.SiteMapProvider = "DepartmentManager"; break;
+                // case SCMSRoleName.ReviewExpert: this.SiteMapDataSource1.SiteMapProvider = "ReviewExpert"; break;
+                // case SCMSRoleName.GuidanceTeacherLeader: this.SiteMapDataSource1.SiteMapProvider = "GuidanceTeacherLeader"; break;
+                // case SCMSRoleName.Project: this.SiteMapDataSource1.SiteMapProvider = "Project"; break;
+                default: this.SiteMapDataSource1.SiteMapProvider = null; break;
+            }
+        }
+
+        protected void tv_PreRender(object sender, EventArgs e)
+        {
+            if (null != Session["SelectedNodeNo"])
+            {
+                int SelectedNodeNo = (int)Session["SelectedNodeNo"];
+                Session.Remove("SelectedNodeNo");
+
+                //折叠其他节点，只展开当前节点
+                if (tv.Nodes[SelectedNodeNo].Parent != null)
+                {
+                    tv.Nodes[SelectedNodeNo].Parent.Expand();
+                    tv.Nodes[SelectedNodeNo].Parent.Selected = true;
+                }
+                tv.Nodes[SelectedNodeNo].Expand();
+                tv.Nodes[SelectedNodeNo].Selected = true;
+            }
+            else if (tv.SelectedNode != null)
+            {
+                //折叠其他节点，只展开当前节点
+                if (tv.SelectedNode.Parent != null)
+                {
+                    tv.SelectedNode.Parent.Expand();
+                    tv.SelectedNode.Expand();
+                }
+                else if (0 == tv.SelectedNode.Depth && tv.SelectedNode.ChildNodes.Count >= 2)
+                {
+                    Response.Redirect("~/Default.aspx");
+                }
+                tv.SelectedNode.Selected = true;
+            }
+        }
+
+        protected void tv_SelectedNodeChanged(object sender, EventArgs e)
+        {
+            if (tv.SelectedNode.Depth == 0)
+            {   //如果是一级目录，需要展开，所以要记录下一级目录的索引
+                Session["SelectedNodeNo"] = tv.Nodes.IndexOf(tv.SelectedNode) == -1 ? 0 : tv.Nodes.IndexOf(tv.SelectedNode);
+            }
+            if (0 == tv.SelectedNode.Depth && tv.SelectedNode.ChildNodes.Count >= 1)
+            {
+                Response.Redirect("~/Default.aspx");
+            }
+            else
+            {
+                Response.Redirect(tv.SelectedNode.NavigateUrl);
+            }
+        }
+
+        protected void tv_TreeNodeDataBound(object sender, TreeNodeEventArgs e)
+        {
+            string urlPattern = e.Node.Value;
+            if (!string.IsNullOrEmpty(urlPattern))
+            {
+                System.Text.RegularExpressions.Regex rgx = new System.Text.RegularExpressions.Regex(urlPattern);
+
+                if (rgx.IsMatch(this.Request.Url.AbsolutePath))
+                {
+                    if (e.Node.Parent != null)
+                    {
+                        e.Node.Parent.Expand();
+                    }
+                    e.Node.Selected = true;
+                }
+            }
+        }
+    }
+}
--- Web/Site.Master.cs
+++ Web/Site.Master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SyncServer;
 
 namespace Web
 {
@@ -12,6 +13,40 @@ namespace Web
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void lblContactName_PreRender(object sender, EventArgs e)
+        {
+            Label lblContactName = (Label)sender;
+            lblContactName.Text = System.Threading.Thread.CurrentPrincipal.Identity.Name;
+            string CurrentUserName = lblContactName.Text;
+
+            SYNCDataContext dc = new SYNCDataContext();
+            string tempRoleName = System.Web.Security.Roles.GetRolesForUser(CurrentUserName).FirstOrDefault();
+            aspnet_Roles aRole = dc.aspnet_Roles.FirstOrDefault(p => p.RoleName.Equals(tempRoleName));
+            if (aRole != null)
+            {
+                lblContactName.ToolTip = aRole.Description;
+                lblContactName.Text = lblContactName.Text + "(" + aRole.Description + ")";
+            }
+        }
+
+        protected void ScriptManager1_AsyncPostBackError(object sender, AsyncPostBackErrorEventArgs e)
+        {
+            if (e.Exception.InnerException != null)
+            {
+                this.ScriptManager1.AsyncPostBackErrorMessage = e.Exception.InnerException.Message;
+            }
+            else
+            {
+                this.ScriptManager1.AsyncPostBackErrorMessage = e.Exception.Message;
+            }
+        }
+
+        protected void HeadLoginStatus_LoggedOut(object sender, EventArgs e)
+        {
+            if (Session.Count > 0)
+                this.Session.RemoveAll();
         }
     }
 }
