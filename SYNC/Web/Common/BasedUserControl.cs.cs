a0da866facfc8a9647009ede8c93f57cdd65540b
xieyan <971387298@qq.com>
2016-01-03 16:33:44 +0800

    change:解决sa登录失败问题

---
+++ SYNC/Web/Common/BasedUserControl.cs.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eIvy.Web.UI.ConfigedControls;
+
+namespace SYNC.Web.Common
+{
+    public class BasedUserControl : BasedConfigedControl
+    {
+        private SYNCDataContext _dataContext;
+        /// <summary>
+        /// 用于使用dbml来来处理数据的上下文对象
+        /// </summary>
+        protected SYNCDataContext DataContext
+        {
+            get
+            {
+                _dataContext = _dataContext ?? new SYNCDataContext();
+                return _dataContext;
+            }
+        }
+
+
+        private string _currentUserName;
+        /// <summary>
+        /// 当前登录用户的名称;
+        /// </summary>
+        protected string CurrentUserName
+        {
+            get
+            {
+                if (HttpContext.Current != null
+                        && HttpContext.Current.User != null
+                            && HttpContext.Current.User.Identity != null
+                                && !string.IsNullOrEmpty(HttpContext.Current.User.Identity.Name))
+                    _currentUserName = HttpContext.Current.User.Identity.Name.Trim();
+                return _currentUserName;
+            }
+        }
+
+
+        private string _currentRoleName;
+        /// <summary>
+        /// 当前登录用户的角色名称;
+        /// </summary>
+        public string CurrentRoleName
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(CurrentUserName))
+                {
+                    string[] r = System.Web.Security.Roles.GetRolesForUser(CurrentUserName);
+                    try
+                    {
+                        switch (r.Length)
+                        {
+                            case 1: _currentRoleName = r[0]; break;
+                            default: _currentRoleName = r.First(); break;
+                        }
+                    }
+                    catch (Exception)
+                    { Response.Redirect("~/Login.aspx"); }
+                }
+                return _currentRoleName;
+            }
+        }
+
+
+        /// <summary>
+        /// 当前登录用户的ID;
+        /// </summary>
+        protected int CurrentEntityID
+        {
+            get
+            {
+                if (this.UserMapping == null) return 0;
+                return Convert.ToInt32(this.UserMapping.EntityID);
+            }
+        }
+
+        private UserMappings _usrMap;
+        /// <summary>
+        /// 该属性用来获取当前登录用户影射的实体信息。
+        /// </summary>
+        public UserMappings UserMapping
+        {
+            get
+            {
+                if (_usrMap == null)
+                {
+                    SYNCDataContext dc = new SYNCDataContext();
+
+                    _usrMap =
+                        dc.UserMappings.FirstOrDefault(
+                        p => p.UserName.Equals(this.Page.User.Identity.Name.Trim()));
+                }
+                return _usrMap;
+            }
+        }
+    }
+}
---
+++ SYNC/Web/Common/UserControl/WUCCurrentPosition.ascx
@@ -0,0 +1,7 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="WUCCurrentPosition.ascx.cs" Inherits="Web.Common.UserControl.WUCCurrentPosition" %>
+<%--<asp:SiteMapPath ID="SiteMapPath1" runat="server">
+</asp:SiteMapPath>--%>
+<asp:Panel ID="pnl_CurrentPosition" runat="server">
+    <asp:Label ID="lblCurrentLocation" runat="server" SkinID="lblCurrentLocation"></asp:Label>
+    <asp:Label ID="lblTitle" runat="server" SkinID="lblTitle"></asp:Label>
+</asp:Panel>
